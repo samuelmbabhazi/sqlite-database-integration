639051914e931daee3463c66eb9f305032b33d2b
Jan Jakes <jan@jakes.pro>
2025-12-10 11:30:40 +0100

    Add a loader file for the MySQL-on-SQLite driver

--- packages/wp-mysql-proxy/src/Adapter/class-sqlite-adapter.php
+++ packages/wp-mysql-proxy/src/Adapter/class-sqlite-adapter.php
@@ -9,24 +9,7 @@ use WP_SQLite_Connection;
 use WP_SQLite_Driver;
 use WP_MySQL_Proxy\MySQL_Protocol;
 
-define( 'SQLITE_DRIVER_PATH', __DIR__ . '/../../../..' );
-
-require_once SQLITE_DRIVER_PATH . '/version.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/parser/class-wp-parser-grammar.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/parser/class-wp-parser.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/parser/class-wp-parser-node.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/parser/class-wp-parser-token.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/mysql/class-wp-mysql-token.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/mysql/class-wp-mysql-lexer.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/mysql/class-wp-mysql-parser.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite/class-wp-sqlite-pdo-user-defined-functions.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite-ast/class-wp-sqlite-connection.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite-ast/class-wp-sqlite-configurator.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite-ast/class-wp-sqlite-driver.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite-ast/class-wp-sqlite-driver-exception.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite-ast/class-wp-sqlite-information-schema-builder.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite-ast/class-wp-sqlite-information-schema-exception.php';
-require_once SQLITE_DRIVER_PATH . '/wp-includes/sqlite-ast/class-wp-sqlite-information-schema-reconstructor.php';
+require_once __DIR__ . '/../../../../wp-pdo-mysql-on-sqlite.php';
 
 class SQLite_Adapter implements Adapter {
 	/** @var WP_SQLite_Driver */
--- wp-includes/sqlite/class-wp-sqlite-db.php
+++ wp-includes/sqlite/class-wp-sqlite-db.php
@@ -315,20 +315,7 @@ class WP_SQLite_DB extends wpdb {
 				return false;
 			}
 
-			require_once __DIR__ . '/../../wp-includes/parser/class-wp-parser-grammar.php';
-			require_once __DIR__ . '/../../wp-includes/parser/class-wp-parser.php';
-			require_once __DIR__ . '/../../wp-includes/parser/class-wp-parser-node.php';
-			require_once __DIR__ . '/../../wp-includes/parser/class-wp-parser-token.php';
-			require_once __DIR__ . '/../../wp-includes/mysql/class-wp-mysql-token.php';
-			require_once __DIR__ . '/../../wp-includes/mysql/class-wp-mysql-lexer.php';
-			require_once __DIR__ . '/../../wp-includes/mysql/class-wp-mysql-parser.php';
-			require_once __DIR__ . '/../../wp-includes/sqlite-ast/class-wp-sqlite-connection.php';
-			require_once __DIR__ . '/../../wp-includes/sqlite-ast/class-wp-sqlite-configurator.php';
-			require_once __DIR__ . '/../../wp-includes/sqlite-ast/class-wp-sqlite-driver.php';
-			require_once __DIR__ . '/../../wp-includes/sqlite-ast/class-wp-sqlite-driver-exception.php';
-			require_once __DIR__ . '/../../wp-includes/sqlite-ast/class-wp-sqlite-information-schema-builder.php';
-			require_once __DIR__ . '/../../wp-includes/sqlite-ast/class-wp-sqlite-information-schema-exception.php';
-			require_once __DIR__ . '/../../wp-includes/sqlite-ast/class-wp-sqlite-information-schema-reconstructor.php';
+			require_once __DIR__ . '/../../wp-pdo-mysql-on-sqlite.php';
 			$this->ensure_database_directory( FQDB );
 
 			try {
